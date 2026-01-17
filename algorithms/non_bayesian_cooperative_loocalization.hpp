f0aa443405d6b1e38b48cbf4b0445349752f1a75
Leonardo Bertolino <leonardo.bertolino@edu.unito.it>
2026-01-18 00:21:47 +0100

    refactoring coop

--- algorithms/non_bayesian_cooperative_loocalization.hpp
+++ algorithms/non_bayesian_cooperative_loocalization.hpp
@@ -11,10 +11,10 @@ namespace coordination {
 FUN vec<2> nBayesianCoop(ARGS, bool is_anchor){ CODE
     vec<2> init = make_vec(node.next_real(0,500), node.next_real(0,500));
     return nbr(CALL, init, [&](field<vec<2>> prev) {
-        field<vec<2>> nbr_pos = node.nbr_vec() + node.position();
+        field<double> nbr_distances = node.nbr_dist();
         field<bool> nbr_anchor = nbr(CALL, is_anchor);
-        tuple<field<vec<2>>, field<vec<2>>, field<bool>> map = make_tuple(nbr_pos, prev, nbr_anchor);
-        auto hop_map_all = list_hood(CALL,  std::vector< tuple<vec<2>, vec<2>, bool> >{}, map);
+        tuple<field<double>, field<vec<2>>, field<bool>> map = make_tuple(nbr_distances, prev, nbr_anchor);
+        auto hop_map_all = list_hood(CALL,  std::vector< tuple<double, vec<2>, bool> >{}, map);
 
         double real_distance, supposed_distance;
         double alpha = 0.1;
@@ -23,12 +23,11 @@ FUN vec<2> nBayesianCoop(ARGS, bool is_anchor){ CODE
         for (auto const& t : hop_map_all) {
             bool anc = get<2>(t);
             if (!is_anchor){
-                vec<2> pos = get<0>(t);
                 vec<2> pos_stim = get<1>(t);
 
-                real_distance = distance(node.position(), pos);
+                real_distance = get<0>(t);
                 supposed_distance = distance(searchPos, pos_stim);
-                int ex = real_distance - supposed_distance;
+                double ex = real_distance - supposed_distance;
 
                 //Vettore direzione dal nodo al vicino
                 vec<2> diff = searchPos - pos_stim;
