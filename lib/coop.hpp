6109d9ca5d97a172b677e4be59bebd0e630effa9
Giorgio Audrito <giorgio.audrito@gmail.com>
2026-01-18 17:29:24 +0100

    pass on coop

--- lib/coop.hpp
+++ lib/coop.hpp
@@ -15,39 +15,25 @@ namespace coordination {
 //! @brief Non-bayesian cooperative localization.
 FUN vec<2> nb_coop(ARGS, bool is_anchor, field<real_t> nbr_dist){ CODE
     vec<2> init = make_vec(node.next_real(0,500), node.next_real(0,500));
-    return nbr(CALL, init, [&](field<vec<2>> prev) {
-        field<bool> nbr_anchor = nbr(CALL, is_anchor);
-        tuple<field<double>, field<vec<2>>, field<bool>> map = make_tuple(nbr_dist, prev, nbr_anchor);
-        auto hop_map_all = list_hood(CALL, std::vector<tuple<double, vec<2>, bool>>{}, map);
-
-        double real_distance, supposed_distance;
-        double alpha = 0.1;
-        vec<2> searchPos = self(CALL, prev);
-
-        for (auto const& t : hop_map_all) {
-            bool anc = get<2>(t);
-            if (!is_anchor){
-                vec<2> pos_stim = get<1>(t);
-
-                real_distance = get<0>(t);
-                supposed_distance = distance(searchPos, pos_stim);
-                double ex = real_distance - supposed_distance;
-
-                //Vettore direzione dal nodo al vicino
-                vec<2> diff = searchPos - pos_stim;
-                double length = norm(diff); // lunghezza del vettore differenza
-                if (length > 0) {
-                    vec<2> dir = diff / length; // direzione unitaria
-                                              // 4. Aggiorna posizione
-                    searchPos += alpha * ex * dir;
-                }
-            }
+    return nbr(CALL, init, [&](field<vec<2>> nbr_pos) {
+        if (is_anchor) return node.position();
+        auto nbr_pos_dist = list_hood(CALL, std::vector<tuple<vec<2>, double>>{}, make_tuple(nbr_pos, nbr_dist));
+        real_t alpha = 0.1;
+        vec<2> pos = self(CALL, nbr_pos);
+        for (auto const& t : nbr_pos_dist) {
+            vec<2> pos_stim = get<0>(t);
+            real_t sensed_dist = get<1>(t);
+            real_t pos_dist = distance(pos, pos_stim);
+            real_t delta = sensed_dist - pos_dist;
+            vec<2> diff = pos - pos_stim;
+            real_t length = norm(diff);
+            if (length > 1e-6) pos += (alpha * delta / length) * diff;
         }
-        return searchPos;
+        return pos;
     });
 }
 //! @brief Export list for coop.
-FUN_EXPORT nb_coop_t = export_list<vec<2>, bool>;
+FUN_EXPORT nb_coop_t = export_list<vec<2>>;
 
 } // namespace coordination
 
